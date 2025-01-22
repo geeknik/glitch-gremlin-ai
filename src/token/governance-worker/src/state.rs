392a74df4e24af362053c8ecd66abec90663c086
Brian Carpenter <geeknik@protonmail.ch>
2025-01-22 17:57:30 -0600

    Governance: Add security level and insurance fund to proposals

--- src/token/governance-worker/src/state.rs
+++ src/token/governance-worker/src/state.rs
@@ -1,11 +1,11 @@
 use borsh_derive::{BorshDeserialize, BorshSerialize};
-use borsh::{BorshDeserialize as _, BorshSerialize as _};
 use solana_program::pubkey::Pubkey;
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct GovernanceProposal {
     pub id: u64,
     pub proposer: Pubkey,
+    #[borsh(skip)]  // Remove description field from on-chain storage
     pub description: String,
     pub target_program: Pubkey,
     pub status: ProposalStatus,
@@ -13,6 +13,10 @@ pub struct GovernanceProposal {
     pub no_votes: u64,
     pub start_time: i64,
     pub end_time: i64,
+    // Added from DESIGN.md 9.3
+    pub security_level: u8,
+    pub execution_delay: i64,
+    pub insurance_fund: Pubkey,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Debug, PartialEq)]
